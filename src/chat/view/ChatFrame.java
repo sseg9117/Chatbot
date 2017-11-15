f6827b1d58fd77206094305a893dc2529546fdc0
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-11-15 08:41:25 -0700

    added a chatframe.java.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,21 +1,29 @@
 package chat.view;
 
-import chat.controller.ChatbotController;
 import javax.swing.JFrame;
+import chat.controller.ChatbotController;
 
 public class ChatFrame extends JFrame
 {
 	
 	private ChatbotController appController;
-
+//	private ChatPanel appPanel;
+	
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
+//		appPanel = new ChatPanel(appController);
+		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setSize(500,500);
+		this.setTitle("GUIFrame Window");
+//		this.setContentPane(appPanel);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
+}
 }
