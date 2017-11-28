57b864bab41e1a7ff065e2c6a610ae0006410188
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-11-28 07:56:25 -0700

    added a switch case

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -136,8 +136,27 @@ public class Chatbot
 			response += "\n" + movieList.get(random).getTitle() + " is a greate movie!";
 		}
 		
-				
+		int followup = (int) (Math.random() * 5);
+		
+		switch (followup)// We will never use this
+		{
+		case 0:
+			response += followUps[0] + "\n";
+			break;
+		case 3:
+			response += followUps[1] + "\n";
+		case 1:
+			response += followUps[2] + "\n";
+			break;
+		default:
+			response += followUps[4] + "\n";
+			response += followUps[3] + "\n";
+			break;
+		}
+		
+		
 		return response;
+	
 	}
 	
 	public boolean lengthChecker(String input)
